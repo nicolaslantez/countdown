f73e083766d3c726d7208362165b7221dd9ce2ef
nicolaslantez <nicolas.lantez96@gmail.com>
2017-03-19 01:54:18 -0300

    Front end conected with MongoDB

--- public/js/validaciones.js
+++ public/js/validaciones.js
@@ -1,19 +1,15 @@
 
 window.onload = function() {	
-	document.getElementById('reportedDate').valueAsDate = new Date();
-	document.getElementById('date').valueAsDate = new Date();
+	document.getElementById('TreatmentDate').valueAsDate = new Date();
+	document.getElementById('Date').valueAsDate = new Date();
 };
 
 $(document).ready(function(){
-	$("#statusSelect").change(function(){
-		console.log($("#statusSelect").val());
-		if($("#statusSelect").val() == "Pendiente"){
+	$("#Status").change(function(){
+		if($("#Status").val() == "Pendiente"){
 			$("#pingDayHidden").show();
 		} else {
 			$("#pingDayHidden").hide();
 		}
 	});
 });
-
-
-
--- server.js
+++ server.js
@@ -1,21 +1,46 @@
 var http = require("http");
 var url = require("url");
 var express = require("express");
+var bodyParser = require("body-parser");
 var mu = require("mu2");
 var fs = require("fs");
 var app = express();
+var mongodb = require("mongodb");
+
+app.use(bodyParser.urlencoded());
 
 app.get('/',function(req,res){
-	mu.clearCache();
-	fs.exists('index.html', function(exists){
-		if(exists) {
-			var stream = mu.compileAndRender('index.html');
-			stream.pipe(res);
+	var MongoClient = mongodb.MongoClient;
+	var url = 'mongodb://localhost:27017/countdownSite';
+
+	MongoClient.connect(url, function(err, db){
+		if(err){
+			console.log('Unable to connect to the server', err);
 		} else {
-			res.writeHead(404);
-			res.end("No existe");
+			console.log('Conection established');
+			var collection = db.collection('entries');
+			collection.find({}).toArray(function(err, result){
+				if(err){
+					res.send(err);
+				} else if(result.length) {
+					mu.clearCache();
+					fs.exists('index.html', function(exists){
+						if(exists) {
+							var stream = mu.compileAndRender('index.html',{results : result});
+							stream.pipe(res);
+						} else {
+							res.writeHead(404);
+							res.end("No existe");
+						}
+					});
+				} else {
+					res.send('No documents found');
+				}
+				db.close();
+			});
 		}
 	});
+
 });
 
 app.get('/newEntry', function(req,res){
@@ -29,6 +54,31 @@ app.get('/newEntry', function(req,res){
 			res.end("No existe");
 		}
 	});
+});
+
+app.post('/addEntry', function(req,res){
+	console.log("ME LLAMANNNNN")
+	var MongoClient = mongodb.MongoClient;
+	var url = 'mongodb://localhost:27017/countdownSite';
+
+	MongoClient.connect(url, function(err, db){
+		if(err){
+			console.log('Unable to connect to the server', err);
+		} else {
+			console.log(req.body);
+			console.log('Conection established');
+			var collection = db.collection('entries');
+			var newEntry = {Date: req.body.Date, IssueID : req.body.IssueID, VulnName : req.body.VulnName, TreatmentDate : req.body.TreatmentDate, WebSecResponsible : req.body.WebSecResponsible, DesaResponsible : req.body.DesaResponsible, DesaProyect : req.body.DesaProyect, Status : req.body.Status, EstimatedDate : req.body.EstimatedDate, Comments : req.body.Comments};
+			collection.insert([newEntry], function(err, result){
+			if(err) {
+				console.log(err);
+			} else {
+				res.redirect("/");
+				}
+			});
+		}
+		db.close();
+	});	
 });
 
 app.use(express.static(__dirname + '/public'));
