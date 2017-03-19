cfeeddb1ef337f81b3a88be6b8f3f3f00ea07fd7
nicolaslantez <nicolas.lantez96@gmail.com>
2017-03-19 13:46:27 -0300

    Automatic IssueID

--- server.js
+++ server.js
@@ -44,16 +44,81 @@ app.get('/',function(req,res){
 });
 
 app.get('/newEntry', function(req,res){
-	mu.clearCache();
-	fs.exists('newEntry.html', function(exists){
-		if(exists) {
-			var stream = mu.compileAndRender('newEntry.html');
-			stream.pipe(res);
+	console.log("ENTRE");
+	var MongoClient = mongodb.MongoClient;
+	console.log("PASE1");
+	var url = 'mongodb://localhost:27017/countdownSite';
+	console.log("PASE2");
+	MongoClient.connect(url, function(err, db){
+		if(err) {
+			console.log('Unable to connect to the server', err);
 		} else {
-			res.writeHead(404);
-			res.end("No existe");
+			console.log('Conection established');
+			var collection = db.collection('entries');
+			collection.find({}).toArray(function(err, result){
+				if(err){
+					res.send(err);
+				} else if(result.length) {
+					mu.clearCache();
+					fs.exists('newEntry.html', function(exists){
+						if(exists) {
+							var issueID = 'Vuln' + result.length;
+							var stream = mu.compileAndRender('newEntry.html', {IssueID : issueID});
+							stream.pipe(res);
+						} else {
+							res.writeHead(404);
+							res.end("No existe");
+						}
+					});
+				} else {
+					res.send('Something was wrong. Please reload');
+				}
+				db.close();
+			});
 		}
 	});
+});
+
+
+app.get('/pings',function(req,res){
+	var MongoClient = mongodb.MongoClient;
+	var url = 'mongodb://localhost:27017/countdownSite';
+
+	MongoClient.connect(url, function(err, db){
+		if(err){
+			console.log('Unable to connect to the server', err);
+		} else {
+			console.log('Conection established');
+			var collection = db.collection('entries');
+			var todayTime = new Date();
+    		var month = todayTime .getMonth() + 1;
+    		// var month = (month < 10) ? '0' + month : month;
+    		var day = todayTime .getDate();
+    		var year = todayTime .getFullYear();
+    		var date = month + "-" + day + "-" + year;
+    		console.log(date);
+					collection.find({Status : "Pendiente", EstimatedDate : {"$lte" : date}}).toArray(function(err, result){
+				if(err){
+					res.send(err);
+				} else if(result.length) {
+					mu.clearCache();
+					fs.exists('index.html', function(exists){
+						if(exists) {
+							var stream = mu.compileAndRender('index.html',{results : result});
+							stream.pipe(res);
+						} else {
+							res.writeHead(404);
+							res.end("No existe");
+						}
+					});
+				} else {
+					res.send('No documents found');
+				}
+				db.close();
+			});
+		}
+	});
+
 });
 
 app.post('/addEntry', function(req,res){
