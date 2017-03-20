cd685bb3fe59c6363673b3c6969d17e19d06e178
nicolaslantez <nicolas.lantez96@gmail.com>
2017-03-19 21:41:19 -0300

    Added the posibility to edit an entry and filter projects that need to be pinged

--- public/js/table.js
+++ public/js/table.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
 	$('#results').DataTable();
 });
+
+$(document).ready(function() {
+    $(".clickable").click(function() {
+        window.location = $(this).data("href");
+    });
+});
--- server.js
+++ server.js
@@ -22,18 +22,22 @@ app.get('/',function(req,res){
 			collection.find({}).toArray(function(err, result){
 				if(err){
 					res.send(err);
-				} else if(result.length) {
+				} else {
 					mu.clearCache();
 					fs.exists('index.html', function(exists){
 						if(exists) {
 							for(element in result){
-								var date = new Date(result[element].EstimatedDate);
-								var month = date .getMonth() + 1;
-	    						var month = (month < 10) ? '0' + month : month;
-	    						var day = date .getDate();
-	    						var year = date .getFullYear();
-	    						date = month + "-" + day + "-" + year;
-	    						result[element].EstimatedDate = date;
+								if(result[element].EstimatedDate != null){
+									var date = new Date(result[element].EstimatedDate);
+									var month = date .getMonth() + 1;
+		    						var month = (month < 10) ? '0' + month : month;
+		    						var day = date .getDate();
+		    						var year = date .getFullYear();
+		    						date = month + "-" + day + "-" + year;
+		    						result[element].EstimatedDate = date;
+								} else {
+									result[element].EstimatedDate = "";
+								}
 							}
 							var stream = mu.compileAndRender('index.html',{results : result});
 							stream.pipe(res);
@@ -42,8 +46,6 @@ app.get('/',function(req,res){
 							res.end("No existe");
 						}
 					});
-				} else {
-					res.send('No documents found');
 				}
 				db.close();
 			});
@@ -64,11 +66,13 @@ app.get('/newEntry', function(req,res){
 			collection.find({}).toArray(function(err, result){
 				if(err){
 					res.send(err);
-				} else if(result.length) {
+				} else {
 					mu.clearCache();
 					fs.exists('newEntry.html', function(exists){
 						if(exists) {
-							var issueID = 'Vuln' + result.length;
+							var issueID;
+							if(result.length) { issueID = 'Vuln' + result.length; }
+							else{ issueID = 'Vuln0'; }
 							var stream = mu.compileAndRender('newEntry.html', {IssueID : issueID});
 							stream.pipe(res);
 						} else {
@@ -76,8 +80,6 @@ app.get('/newEntry', function(req,res){
 							res.end("No existe");
 						}
 					});
-				} else {
-					res.send('Something was wrong. Please reload');
 				}
 				db.close();
 			});
@@ -100,7 +102,7 @@ app.get('/pings',function(req,res){
 			collection.find({Status : "Pendiente", EstimatedDate : {$lte : date}}).toArray(function(err, result){
 				if(err){
 					res.send(err);
-				} else if(result.length) {
+				} else {
 					mu.clearCache();
 					fs.exists('index.html', function(exists){
 						if(exists) {
@@ -120,8 +122,38 @@ app.get('/pings',function(req,res){
 							res.end("No existe");
 						}
 					});
+				}
+				db.close();
+			});
+		}
+	});
+});
+
+app.get('/:issueID', function(req,res){
+	var MongoClient = mongodb.MongoClient;
+	var url = 'mongodb://localhost:27017/countdownSite';
+	MongoClient.connect(url, function(err, db){
+		if(err) {
+			console.log('Unable to connect to the server', err);
+		} else {
+			console.log('Conection established');
+			var collection = db.collection('entries');
+			collection.find({IssueID : req.params.issueID}).toArray(function(err, result){
+				if(err){
+					res.send(err);
+				} else if(result.length) {
+					mu.clearCache();
+					fs.exists('edit.html', function(exists){
+						if(exists) {
+							var stream = mu.compileAndRender('edit.html', {vuln : result});
+							stream.pipe(res);
+						} else {
+							res.writeHead(404);
+							res.end("No existe");
+						}
+					});
 				} else {
-					res.send('No documents found');
+					res.send('Something was wrong. Please reload');
 				}
 				db.close();
 			});
@@ -137,12 +169,12 @@ app.post('/addEntry', function(req,res){
 		if(err){
 			console.log('Unable to connect to the server', err);
 		} else {
-			// console.log(req.body);
 			console.log('Conection established');
 			var collection = db.collection('entries');
-			// console.log(req.body.Date);
-			// console.log(new Date(req.body.Date));
-			var newEntry = {Date: req.body.Date, IssueID : req.body.IssueID, VulnName : req.body.VulnName, TreatmentDate : req.body.TreatmentDate, WebSecResponsible : req.body.WebSecResponsible, DesaResponsible : req.body.DesaResponsible, DesaProyect : req.body.DesaProyect, Status : req.body.Status, EstimatedDate : new Date(req.body.EstimatedDate), Comments : req.body.Comments};
+			var date;
+			var date = (!req.body.EstimatedDate) ? date = null : date = new Date(req.body.EstimatedDate);
+			console.log(date);
+			var newEntry = {Date: req.body.Date, IssueID : req.body.IssueID, VulnName : req.body.VulnName, TreatmentDate : req.body.TreatmentDate, WebSecResponsible : req.body.WebSecResponsible, DesaResponsible : req.body.DesaResponsible, DesaProyect : req.body.DesaProyect, Status : req.body.Status, EstimatedDate : date, Comments : req.body.Comments};
 			collection.insert([newEntry], function(err, result){
 			if(err) {
 				console.log(err);
@@ -150,6 +182,33 @@ app.post('/addEntry', function(req,res){
 				res.redirect("/");
 				}
 			});
+		}
+		db.close();
+	});	
+});
+
+app.post('/editEntry', function(req,res){
+	var MongoClient = mongodb.MongoClient;
+	var url = 'mongodb://localhost:27017/countdownSite';
+
+	MongoClient.connect(url, function(err, db){
+		if(err){
+			console.log('Unable to connect to the server', err);
+		} else {
+			console.log('Conection established');
+			var collection = db.collection('entries');
+			collection.update({IssueID : req.body.IssueID},{$set : {
+				"Date" : req.body.Date,
+				"VulnName" : req.body.VulnName,
+				"TreatmentDate" : req.body.TreatmentDate,
+				"WebSecResponsible" : req.body.WebSecResponsible,
+				"DesaResponsible" : req.body.DesaResponsible,
+				"DesaProyect" : req.body.DesaProyect,
+				"Status" : req.body.Status,
+				"EstimatedDate" : req.body.EstimatedDate,
+				"Comments" : req.body.Comments
+			}});
+			console.log("Entry updated");
 		}
 		db.close();
 	});	
